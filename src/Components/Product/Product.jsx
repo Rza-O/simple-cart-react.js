9a53bb85dabd09225741fb07058e306c35daa00a
appoclayptor <shahreza1414@gmail.com>
2024-10-24 04:27:32 +0600

    Showed all products in cards

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -5,10 +5,18 @@ Product.propTypes = {
 };
 
 function Product({ product }) {
+    const { id, name, image, description, price, isFeature } = product
     console.log(product);
     return (
-        <div>
-            <h1>Single Product</h1>
+        <div className='w-80 border-2 border-solid border-gray-400 m-4 p-4 text-center shadow-xl rounded-lg'>
+            <img className='mb-4 h-64 w-full' src={image} alt="" />
+            <h2 className='text-2xl font-bold mb-4' >{name}</h2>
+            <p className='mb-4'>{description}</p>
+            <div className='space-y-3 p-1'>
+                <p className='font-extrabold'>${price}</p>
+                <p className='font-extralight'>{isFeature? 'Featured' : 'Not Featured'}</p>
+            </div>
+            <button className='btn bg-yellow-200 p-2 rounded-lg font-semibold'> Add to Cart</button>
         </div>
     );
 }
